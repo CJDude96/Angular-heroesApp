8c2198ed6feead49d529862e9677da55221decd8
Carlos Sanchez <cjsanche.dev@gmail.com>
2024-03-20 19:16:09 -0400

    hero list card

--- src/app/heroes/components/card/card.component.ts
+++ src/app/heroes/components/card/card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
-  selector: 'app-card',
+  selector: 'heroes-hero-card',
   templateUrl: './card.component.html',
   styles: ``
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  @Input()
+  public hero!: Hero
+
+  ngOnInit(): void {
+    if( !this.hero ) throw Error('Hero property is required')
+  }
 
 }
