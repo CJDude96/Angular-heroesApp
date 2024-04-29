b23b105189fd7c8ed9845027926fe354c3eeb7e6
Carlos Sanchez <cjsanche.dev@gmail.com>
2024-04-29 18:42:15 -0400

    search autocomplete

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SearchPageComponent {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
 
-  public searchInput = new FormControl('')
-  public heroes: Hero[] = []
+  constructor(private heroesService: HeroesService, private router: Router) {}
 
-  searchHero() {
-    const value: string = this.searchInput.value || ''
-    
+  searchHero(): void {
+    const value: string = this.searchInput.value || '';
+    this.heroesService
+      .getSuggestions(value)
+      .subscribe((arg) => (this.heroes = arg));
   }
 
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void {
+    if (!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.router.navigate([`/heroes/${hero.id}`]);
+  }
 }
